145cda011696b58fddb67d67a37b1cfecdd6e18a
NextKolya <phelixgluy@gmail.com>
2026-01-30 22:49:38 +0600

    add docs for generateHtml

--- src/generateHtml/generateHtml.ts
+++ src/generateHtml/generateHtml.ts
@@ -3,9 +3,39 @@ import type { MarkdownCSSClasses } from '../generator/types';
 import { parse } from '../parser';
 import { generate } from '../generator';
 
+/**
+ * #### Generates HTML from provided markdown string.
+ *
+ * @param {string} source String with MarkDown.
+ * @param {MarkdownCSSClasses | undefined} cssClasses Object with CSS classes for MarkDown HTML elements.
+ *
+ *  @returns {string} Generated HTML from provided markdown string.
+ *
+ *
+ *
+ * @example
+ * ```typescript
+ * generateHtml('### heading 3 **bold**', { heading: 'heading-classname', bold: 'bold-class' });
+ * ```
+ *
+ * Output:
+ *
+ * ```html
+ * <h3 class="heading-classname">heading 3 <strong class="bold-class">bold</strong>
+ * ```
+ *
+ * @example
+ * ```typescript
+ * const markdownContainer = document.querySelector('.markdown-container');
+ *
+ * const input = document.querySelector('input');
+ * input.addEventListener('input', (event) => {
+ *   markdownContainer.setHTMLUnsafe(generateHTMl(event.target.value, {}));
+ * })
+ */
 export const generateHtml = (
     source: string,
-    cssClasses: MarkdownCSSClasses,
-) => {
-    return generate(parse(source), cssClasses);
+    cssClasses?: MarkdownCSSClasses,
+): string => {
+    return generate(parse(source, 0, source.length).body, cssClasses ?? {});
 };
