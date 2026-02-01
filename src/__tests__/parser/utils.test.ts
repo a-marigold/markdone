27a4304190b47fbd0ad0d65098767cc521f38d6d
NextKolya <phelixgluy@gmail.com>
2026-02-01 20:02:50 +0600

    add tests for checkStartOfLine util

--- src/__tests__/parser/utils.test.ts
+++ src/__tests__/parser/utils.test.ts
@@ -1,23 +1,58 @@
 import { describe, it, expect } from 'bun:test';
 
-import { checkHasContent } from '../../parser/utils';
+import { checkHasContent, checkStartOfLine } from '../../parser/utils';
 
 describe('checkHasContent', () => {
     it('should work only with place from received `start` to `end` arguments', () => {
-        const string = ' abcdef \r\n';
+        const source = ' abcdef \r\n';
 
-        expect(checkHasContent(string, 1, string.length)).toBe(true);
-        expect(checkHasContent(string, 0, 1)).toBe(false);
-        expect(checkHasContent(string, 8, string.length)).toBe(false);
+        expect(checkHasContent(source, 1, source.length)).toBe(true);
+
+        expect(checkHasContent(source, 0, 1)).toBe(false);
+        expect(checkHasContent(source, 8, source.length)).toBe(false);
     });
 
     it('should return false if the `source` contains only empty characters', () => {
-        const string = '\t\t\t\t \n \r\n \r\n \n     ';
-        expect(checkHasContent(string, 0, string.length)).toBe(false);
+        const source = '\t\t\t\t \n \r\n \r\n \n     ';
+        expect(checkHasContent(source, 0, source.length)).toBe(false);
     });
 
-    it('should return true if the `source` contains any not empty character', () => {
-        const string = '\t abcdef \r\n';
-        expect(checkHasContent(string, 0, string.length)).toBe(true);
+    it('should return true if the `source` contains any non empty character', () => {
+        const source = '\t abcdef \r\n';
+
+        expect(checkHasContent(source, 0, source.length)).toBe(true);
+    });
+});
+
+describe('checkNewLine', () => {
+    it('should return true if `pos` is in the start of `source`', () => {
+        const source = '    # abcde';
+
+        const pos = 1;
+        expect(checkStartOfLine(source, 0, pos)).toBe(true);
+    });
+
+    it('should skip spaces and tabs while `pos` is more than `sourceStart`', () => {
+        const source = '\t\t\t   ';
+
+        const pos = source.length - 1;
+
+        expect(checkStartOfLine(source, 0, pos)).toBe(true);
+    });
+
+    it('should return false if there is any non empty character between `pos` and Line Feed character', () => {
+        const source = '\nbad text ';
+
+        const pos = 3;
+
+        expect(checkStartOfLine(source, 0, pos)).toBe(false);
+    });
+
+    it('should node handle part of `source` that is less than `minPos` argument', () => {
+        const source = 'start text';
+        const minPos = 5;
+        const pos = 5;
+
+        expect(checkStartOfLine(source, minPos, pos)).toBe(true);
     });
 });
